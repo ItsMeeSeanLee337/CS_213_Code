6cde3c81b6f88755bd8bb0c46a894212bc99be93
Sean Lee <slee206061@gmail.com>
2023-02-07 13:33:05 -0500

    Updated

---
+++ HelloFX/src/App.java
@@ -0,0 +1,13 @@
+package hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
